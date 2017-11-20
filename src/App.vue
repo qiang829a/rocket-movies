a1384a7c2c83edd1aa3c128e8657febec58f7ead
study <853655312@qq.com>
2017-11-20 14:14:06 +0800

    asdf

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <router-view class="view" keepalive></router-view>
+    </keep-alive>
+    <movies-footer class="footer"></movies-footer>
+  </div>
+</template>
+
+<script type="text.ecmascript-6">
+  import MoviesFooter from 'components/footer/footer'
+  export default {
+    components: {
+      MoviesFooter
+    }
+  }
+</script>
+
+<style lang="scss">
+  html,body,#app{
+    height: 100%; overflow: hidden;
+  }
+  #app{
+    .view{
+      position: absolute; left: 0; right: 0; top: 0; bottom: 3.57rem; margin-bottom: 1px;
+    }
+    .footer{
+      position: fixed; left: 0; right: 0; bottom: 0; z-index: 200;
+    }
+  }
+</style>
---
+++ src/components/footer/footer.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <div class="footer">
+    <router-link to="/home">
+      <dl>
+        <dd class="movies"></dd>
+        <dt>进入影院</dt>
+      </dl>
+    </router-link>
+    <router-link to="/a">
+      <dl>
+        <dd class="make-money"></dd>
+        <dt>赚钱</dt>
+      </dl>
+    </router-link>
+    <router-link to="/extension">
+      <dl>
+        <dd class="extension-mk"></dd>
+        <dt>推广赚钱</dt>
+      </dl>
+    </router-link>
+    <router-link to="/user">
+      <dl>
+        <dd class="vip"></dd>
+        <dt>会员中心</dt>
+      </dl>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~common/scss/variable";
+  .footer{
+    position: relative; display: flex; height: 3.57rem; border-top: 1px solid #dedede; background: $color-text;
+    a{
+      flex: 1;
+      dd{
+        width: 1.61rem; height: 1.5rem; margin: 0.5rem auto 0; background-position: center; background-repeat: no-repeat; background-size: contain;
+        &.movies{
+          background-image: url(movies.png);
+        }
+        &.make-money{
+          background-image: url(money.png);
+        }
+        &.extension-mk{
+          background-image: url(qr_code.png);
+        }
+        &.vip{
+          background-image: url(vip.png);
+        }
+      }
+      dt{
+        height: 1.5rem; line-height: 1.5rem; font-size: $font-size-small-s; color: $color-text-g; text-align: center;
+      }
+      &.router-link-active{
+        .movies{
+          background-image: url(movies_on.png);
+        }
+        dt{
+          color: $color-text-c;
+        }
+        .make-money{
+          background-image: url(money_on.png);
+        }
+        .extension-mk{
+          background-image: url(qr_code_on.png);
+        }
+        .vip{
+          background-image: url(vip_on.png);
+        }
+      }
+    }
+  }
+</style>
